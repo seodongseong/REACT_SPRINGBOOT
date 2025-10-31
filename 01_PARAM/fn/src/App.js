fb69fb5d06e0ebb5325a386db08bde88791cad25
seodongseong <ehdtjd0821@naver.com>
2025-10-31 11:03:48 +0900

    10-31

---
+++ 01_PARAM/fn/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Param from './components/Param'
+function App() {
+  return (
+    <div className="App">
+        <Param />
+    </div>
+  );
+}
+
+export default App;
